b3d00907d4c57c929577e9d9f0fd5898b6573169
YeaChan05 <qkenrdl05@gmail.com>
2023-02-01 14:51:42 +0900

    baekjoon 2475

---
+++ Main.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        input(list);
+        System.out.println(calc(list));
+    }
+
+    public static void input(List<Integer> list) {
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < 5; i++) {
+            int value = scanner.nextInt();
+            if (value > 0 && value < 10) list.add(value);
+        }
+    }
+
+    private static int calc(List<Integer> list) {
+        int temp=0;
+        for (int ele:list) {
+            temp+=Math.pow(ele,2);
+        }
+         return temp%10;
+    }
+}
