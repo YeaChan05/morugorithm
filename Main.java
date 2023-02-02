3d25a43726e3efd26ee3b2ef87cd54e887e78cf2
YeaChan05 <qkenrdl05@gmail.com>
2023-02-02 22:05:32 +0900

    baekjoon 2231

--- Main.java
+++ Main.java
@@ -2,43 +2,32 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    static int a;
-    static int b;
-    static int c;
-    static private BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
-
-
     public static void main(String[] args) throws IOException {
-        List<String> answer = new ArrayList<String>();
-        while (true) {
-            input();
-            if (a == 0 && b == 0 && c == 0) break;
-            answer.add(calc());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int answer = 0;
+        for (int i = 1; i < n; i++) {
+            int num = calc(i);
+            if (num == n) {
+                answer = i;
+                break;
+            }
         }
-        ;
-        answer.forEach(System.out::println);
+        System.out.print(answer);
     }
 
-
-    private static String calc() {
-        if ((Math.pow(a, 2) + Math.pow(b, 2) == Math.pow(c, 2))
-                || (Math.pow(b, 2) + Math.pow(c, 2) == Math.pow(a, 2))
-                || (Math.pow(c, 2) + Math.pow(a, 2) == Math.pow(b, 2)))
-            return "right";
-
-        return "wrong";
-    }
-
-    private static void input() throws IOException {
-        String str = reader.readLine();
-        String[] ele = str.split(" ");
-        a = Integer.parseInt(ele[0]);
-        b = Integer.parseInt(ele[1]);
-        c = Integer.parseInt(ele[2]);
+    static int calc(int n) {
+        int temp = n;
+        while (n != 0) {
+            temp += n % 10;
+            n /= 10;
+        }
+        return temp;
     }
 }
