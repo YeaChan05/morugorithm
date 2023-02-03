2b70cf2ddd0548be759cc0fabbc4c334c4992388
YeaChan05 <qkenrdl05@gmail.com>
2023-02-03 10:02:27 +0900

    Retrieve records

--- Main2231.java
+++ Main2231.java
@@ -1,12 +1,9 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
-public class Main {
+public class Main2231 {
     public static void main(String[] args) {
         List<Integer> list = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
---
+++ Main4153.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main4153 {
+    static int a;
+    static int b;
+    static int c;
+    static private BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+
+
+    public static void main(String[] args) throws IOException {
+        List<String> answer = new ArrayList<String>();
+        while (true) {
+            input();
+            if (a == 0 && b == 0 && c == 0) break;
+            answer.add(calc());
+        }
+        ;
+        answer.forEach(System.out::println);
+    }
+
+
+    private static String calc() {
+        if ((Math.pow(a, 2) + Math.pow(b, 2) == Math.pow(c, 2))
+                || (Math.pow(b, 2) + Math.pow(c, 2) == Math.pow(a, 2))
+                || (Math.pow(c, 2) + Math.pow(a, 2) == Math.pow(b, 2)))
+            return "right";
+
+        return "wrong";
+    }
+
+    private static void input() throws IOException {
+        String str = reader.readLine();
+        String[] ele = str.split(" ");
+        a = Integer.parseInt(ele[0]);
+        b = Integer.parseInt(ele[1]);
+        c = Integer.parseInt(ele[2]);
+    }
+}
