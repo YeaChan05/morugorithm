1a6b1bf3bf873015defc227ae8ef536654be7cbd
YeaChan05 <qkenrdl05@gmail.com>
2023-02-05 17:25:51 +0900

    baekjoon 2609: another solution

--- Main2609.java
+++ Main2609.java
@@ -1,9 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
 
 public class Main2609 {
     public static void main(String[] args) throws IOException {
@@ -13,34 +10,22 @@ public class Main2609 {
         int first=Integer.parseInt(input.split(" ")[0]);
         int second=Integer.parseInt(input.split(" ")[1]);
 
-        List<Integer> firstPrimeNumber = new ArrayList<>();
-        List<Integer> secondPrimeNumber = new ArrayList<>();
 
-        factorization(first,firstPrimeNumber);
-        factorization(second, secondPrimeNumber);
-
-        int gfc=GFC(firstPrimeNumber,secondPrimeNumber);
+        int gfc=GFC(first,second);
         System.out.println(gfc);
         System.out.println((first*second)/gfc);
     }
 
 
-    private static int GFC(List<Integer> firstPrimeNumber, List<Integer> secondPrimeNumber) {
-        List<Integer> common = new ArrayList<>();
-        for (Integer value : firstPrimeNumber) {
-            for (Integer integer : secondPrimeNumber) {
-                if (Objects.equals(value, integer)) {
-                    common.add(value);
-                    break;
-                }
+    private static int GFC(int first, int second) {
+        int temp= Math.min(first, second);
+        for (int i = temp; i > 0; i--) {
+            if((first%i==0)&&(second%i==0)) {
+                return i;
             }
         }
-        return common.get(common.size()-1);
+        return 1;
     }
 
-    private static void factorization(int num, List<Integer> primeNumber) {
-        for (int i = 1; i <=num ; i++)
-            if(num % i == 0)
-                primeNumber.add(i);
-    }
+
 }
